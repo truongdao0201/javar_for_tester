d66fafa695ae2f6785791a2bdab3fa476f430994
daohuytruong <truongdao0201@gmail.com>
2023-09-25 14:38:40 +0700

    Bai tap 5 ve long vap for

---
+++ src/BT5_For/bai_for.java
@@ -0,0 +1,9 @@
+package BT5_For;
+
+public class bai_for {
+    public static void main(String[] args) {
+        for(int i=0; i <= 50; i++){
+            System.out.println("Số thứ tự: " + i);
+        }
+    }
+}
