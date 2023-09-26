8e891ca88ce7cf2f75c7658810053adc1ea5cd7b
daohuytruong <truongdao0201@gmail.com>
2023-09-26 09:25:15 +0700

    Bo sung bai For cai tien, While, Do While

--- src/BT5_For/bai_for.java
+++ src/BT5_For/bai_for.java
@@ -2,8 +2,26 @@ package BT5_For;
 
 public class bai_for {
     public static void main(String[] args) {
-        for(int i=0; i <= 50; i++){
+        System.out.println("Bài tập về For đơn giản"); // Bài tập For cải tiến
+        for(int i = 0; i <= 50; i++){
             System.out.println("Số thứ tự: " + i);
         }
+        System.out.println("\n Bài tập về For cải tiến");// Bài tập For cải tiến
+        String arrayName[] = {"truong", "dat", "huy", "hung", "han"};
+        for (String name: arrayName){
+            System.out.println(name);
+        }
+        System.out.println("\n Bài tập về While"); // Bài tập về while
+        int i=0;
+        while (i < 10){
+            System.out.println("Số thứ tự: " + i);
+            i++;
+        }
+        System.out.println("\n Bài tập về Do While"); // Bài tập do while
+        int number = 0;
+        do {
+            System.out.println("Số thứ tự: " + number);
+            number++;
+        }while (number <= 10);
     }
 }
