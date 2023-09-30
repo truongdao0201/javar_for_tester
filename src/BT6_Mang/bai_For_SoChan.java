20b8c2f511225b9ccf19cf9389668b66cb74f0d2
daohuytruong <truongdao0201@gmail.com>
2023-09-30 13:19:15 +0700

    cập nhật lại ghi chú

--- src/BT6_Mang/bai_For_SoChan.java
+++ src/BT6_Mang/bai_For_SoChan.java
@@ -2,7 +2,7 @@ package BT6_Mang;
 
 public class bai_For_SoChan {
     public static void main(String[] args) {
-        System.out.println("--------- Mảng Tìm Số Chẳn ---------");
+        System.out.println("--------- Mảng các số nguyên ---------");
         int number[] = new int[50];// Bài tập For và mảng in số chẳn
         for (int i=0; i < number.length; i++){
             number[i] = i+1;
